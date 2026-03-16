5108240842611405aa243552ec45ddf02ea2c3f1
Sameer Sethar <sameersethar907@gmail.com>
2026-03-16 16:42:44 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -135,6 +135,7 @@ gdal_version = _get_gdal_version()
 req_file = 'requirements.txt'
 requirements = [l for l in open(req_file).readlines() if l and not l.startswith('#')]
 requirements += ['GDAL=={}'.format(gdal_version)]
+
 setup(
     name='lisflood-model',
     version=version,
@@ -184,3 +185,7 @@ setup(
         'testpypi': UploadCommandTest,
     },
 )
+
+
+
+
